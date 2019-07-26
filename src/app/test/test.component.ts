5b29314ef748a02712f5191202aee6b7ec0ec92e
mittulmandhan <conanmittul@gmail.com>
2019-07-27 01:58:09 +0530

    class binding completed

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  public name = 'Mittul';
+  public successClass = 'text-success';
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial,
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
